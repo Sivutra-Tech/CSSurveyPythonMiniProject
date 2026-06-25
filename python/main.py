e79470f142983eceaa0cb9163e61c4f44048759f
Sivutra-Tech <240019633+Sivutra-Tech@users.noreply.github.com>
2026-06-25 08:57:57 +0000

    Copying from Colab and Having Printing Seats

--- python/main.py
+++ python/main.py
@@ -1,14 +1,40 @@
-print("hello world")
+#Pick Genre
+def addGenres(listOfGenres, whatToBeAdded):
+  listOfGenres.append(whatToBeAdded)
 
-for i in range(10):
-    print(i)
+def printGenres(listOfGenres):
+  num = 1
+  for genre in listOfGenres:
+    print(f"{num}. {genre} ")
+    num+=1
 
-while True:
-    break
-print("Something")
+listOfGenres = ["Romantic","Action","Horror","Sci-Fi","Fantasy"]
+printGenres(listOfGenres)
 
-print("sdauhdu")
+# Printing Seats
+allOfTheSeat = [
+    [1,2,3,4,5,6,7,8,9,10], #A (1.Num) (1.9 = A9)
+    [1,2,3,4,5,6,7,8,9,10], #B
+    [1,2,3,4,5,6,7,8,9,10], #C
+    [1,2,3,4,5,6,7,8,9,10], #D
+    [1,2,3,4,5,6,7,8,9,10], #E
+    [1,2,3,4,5,6,7,8,9,10], #F
+    [1,2,3,4,5,6,7,8,9,10], #G
+    [1,2,3,4,5,6,7,8,9,10], #H
+    [1,2,3,4,5,6,7,8,9,10], #I
+    [1,2,3,4,5,6,7,8,9,10], #J
+]
 
-print("Asasoaisiaosa")
+def printSeat():
+  letter = ["A","B","C","D","E","F","G","H","I","J"]
+  tempNumLetter = 0
+  for row in allOfTheSeat:
+    print(f"{letter[tempNumLetter]}: ", end="")
+    for colum in row:
+      print(f"[{colum}]", end=" ")
+    print("\n")
+    tempNumLetter+=1
+def pickSeat(row,colum):
 
-print("audadhuuh")
+
+printSeat()
