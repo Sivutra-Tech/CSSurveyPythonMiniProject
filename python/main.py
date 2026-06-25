8d03b2150e1b6b38e8f9429bb8e6b22a877e7925
Sivutra-Tech <240019633+Sivutra-Tech@users.noreply.github.com>
2026-06-25 13:14:11 +0000

    reorganized the code

--- python/main.py
+++ python/main.py
@@ -1,27 +1,14 @@
 import functions
+import movieData as movie
+import seatData as seat
 
 
 #Pick Genre
 
-listOfGenres = ["Romantic","Action","Horror","Sci-Fi","Fantasy"]
-functions.printGenres(listOfGenres)
-
-# Printing Seats
-allOfTheSeat = [
-    [1,2,3,4,5,6,7,8,9,10], #A (1.Num) (1.9 = A9)
-    [1,2,3,4,5,6,7,8,9,10], #B
-    [1,2,3,4,5,6,7,8,9,10], #C
-    [1,2,3,4,5,6,7,8,9,10], #D
-    [1,2,3,4,5,6,7,8,9,10], #E
-    [1,2,3,4,5,6,7,8,9,10], #F
-    [1,2,3,4,5,6,7,8,9,10], #G
-    [1,2,3,4,5,6,7,8,9,10], #H
-    [1,2,3,4,5,6,7,8,9,10], #I
-    [1,2,3,4,5,6,7,8,9,10], #J
-]
+functions.printGenres(movie.getGenre())
 
 
-functions.printSeat(allOfTheSeat)
+functions.printSeat(seat.getSeat())
 
 horror_movie= [
     "The conjuring","IT","The Exorcist", "The shining", "Halloween",
