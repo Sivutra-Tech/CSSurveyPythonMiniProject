37e71d00d48fc26df08fa0449135e5c982e960c1
minsivutra@gmail.com <minsivutra@gmail.com>
2026-06-27 12:13:13 +0700

    adding addMovie() removeMovie() and getMovie()

--- python/movieData.py
+++ python/movieData.py
@@ -19,16 +19,50 @@ sci_fi_movie= [
     "Space Odyssey", "Planet of the Apes", "Alien", "Back to the Future", "Star Wars: The Empire Strikes Back"
 ]
 
-def getListOfMovie(genre):
-    pass
-
 def getGenre():
     listOfGenres = ["Romantic","Action","Horror","Sci-Fi","Fantasy"]
     return listOfGenres
 
-def appendMovie():
-    pass
+def getMovie(genre):
+    with open(fileLocation,"r") as movieData:
+        data = csv.reader(movieData)
+        resultMovieList = []
+        for row in data:
+            if row[1] == genre:
+                print(row)
+                resultMovieList.append(row)
+        
+    return resultMovieList
+                
 
-def removeMovie():
-    pass
+        
 
+
+# Adding and Removing Movies
+import csv
+
+fileLocation = './python/data.csv'
+
+def addMovie(MovieID,MovieGenre,MovieName,MovieDuration,MovieTime1,MovieTime2,MovieDate1,MovieDate2):
+    with open(fileLocation,"a") as movieData:
+        # movie = ['Movie_ID', 'Movie_Genre', 'Movie_Name', 'Movie_Duration', 'Movie_Time_1', 'Movie_Time_2', 'Movie_Date_1', 'Movie_Date_2']
+
+        movie = [MovieID,MovieGenre,MovieName,MovieDuration,MovieTime1,MovieTime2,MovieDate1,MovieDate2]
+        data = csv.writer(movieData)
+
+        data.writerow(movie)
+
+def removeMovie(movieID):
+    with open(fileLocation,'r',newline='') as movieData:
+        reader = csv.reader(movieData)
+        bigAllMovie = []
+        for row in reader:
+            if row[0] == movieID:
+                continue
+            bigAllMovie.append(row)
+
+    with open(fileLocation,"w") as movieData:
+        writer = csv.writer(movieData)
+        for row in bigAllMovie:
+            writer.writerow(row)
+    
