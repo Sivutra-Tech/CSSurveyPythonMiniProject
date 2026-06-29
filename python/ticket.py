225416ea809d6172e4040e2bc109b32d664f80e0
minsivutra@gmail.com <minsivutra@gmail.com>
2026-06-29 21:59:25 +0700

    making the request ticket

--- python/ticket.py
+++ python/ticket.py
@@ -1,6 +1,8 @@
 import bookAMovie
 import random
 
+testList = ["12-June-2026-123",]
+
 
 def generateTicket():
     someList = bookAMovie.getComfirmationList()
@@ -9,3 +11,31 @@ def generateTicket():
 
     bookAMovie.setComfirmationList(someList,action="update")
 
+
+import csv
+fileLocation = "./python/ticketStorage.csv"
+def addTicketToStorage(comfirmationList):
+    with open(fileLocation,"w") as file:
+        writer = csv.writer(file)
+        writer.writerow(comfirmationList)
+
+def searchTicket(ticketID):
+    with open(fileLocation,"r") as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if row[0] == ticketID:
+                print("eh?")
+                print(row)
+                print("eh?")
+                return row
+            
+        return None
+
+
+addTicketToStorage(["12-June-2026-213","The Avenger"])
+
+result = searchTicket("12-June-2026-213")
+if result:
+    print(result)
+else:
+    print("Ticket Not Found")
